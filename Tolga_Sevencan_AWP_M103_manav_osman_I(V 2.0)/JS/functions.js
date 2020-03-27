3055a69b7da82847ce89bd3732925d53cf1152e8
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-03-27 07:48:25 +0100

    working

--- Tolga_Sevencan_AWP_M103_manav_osman_I(V 2.0)/JS/functions.js
+++ Tolga_Sevencan_AWP_M103_manav_osman_I(V 2.0)/JS/functions.js
@@ -1,4 +1,14 @@
 let urunler = ["Ananas", "Biber", "Domates", "Muz"];
 
+let kap = document.querySelector('#vitrin');
+
 let ul = document.createElement('ul');
 
+document.body.appendChild(kap);
+kap.appendChild(ul);
+
+for (let i=0; i<urunler.length; i++) {
+    let li = document.createElement('li');
+    ul.appendChild(li);
+    li.innerHTML=li.innerHTML + urunler[i];
+}
