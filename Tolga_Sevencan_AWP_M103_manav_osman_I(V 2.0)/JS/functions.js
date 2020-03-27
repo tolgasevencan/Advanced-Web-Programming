ded1154dbf5217775229fb7c551506aeab473d8f
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-03-27 06:30:26 +0100

    go on

--- Tolga_Sevencan_AWP_M103_manav_osman_I(V 2.0)/JS/functions.js
+++ Tolga_Sevencan_AWP_M103_manav_osman_I(V 2.0)/JS/functions.js
@@ -1,11 +1,4 @@
-/*
-function addButtonToTable(button, table) {
-    let cell = document.createElement("td");
-        //Get first row of table
-    let row = table.rows[0];
-}
-*/
-function secimFonksiyonu() {
-    let sec = document.querySelector(".sebzeListe").rows[0].cells.length;
-    document.querySelector("#secilenler").innerHTML = sec;
-};
+let urunler = ["Ananas", "Biber", "Domates", "Muz"];
+
+let ul = document.createElement('ul');
+
