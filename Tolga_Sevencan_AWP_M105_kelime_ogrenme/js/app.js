9d2ac018562883290e0635959aef61d0a49cf5a6
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-03-30 09:33:03 +0200

    continue

---
+++ Tolga_Sevencan_AWP_M105_kelime_ogrenme/js/app.js
@@ -0,0 +1,3 @@
+let germanWords = ["spielen", "reden", "die Tatsache", "der Witz", "das Protokoll", "das Herz", "das Kind", "codierung"];
+
+let englishWords = ["to go", "to wash", "to play", "to argue"];
