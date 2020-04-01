8eb643aff4a3fda44c968d98d0f9fa5b661ba5fb
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-04-01 12:24:08 +0200

    new

--- Tolga_Sevencan_AWP_M105_kelime_ogrenme/js/app.js
+++ Tolga_Sevencan_AWP_M105_kelime_ogrenme/js/app.js
@@ -2,22 +2,21 @@ let germanWords = ["spielen"];
 
 let englishWords = [
     {
-        "choice": "A)",
-        "text": "to go"
+        choice: "A)",
+        text: "to go"
     },
     {
-        "choice": "B)",
-        "text": "to play"
+        choice: "B)",
+        text: "to play"
     },
     {
-        "choice": "C",
-        "text": "to argue"
+        choice: "C",
+        text: "to argue"
     },
     {
-        "choice": "D",
-        "text": "to walk"
+        choice: "D",
+        text: "to walk"
     }
 ];
 
-
-
+document.querySelector('.options').innerHTML = englishWords.choice + text;
