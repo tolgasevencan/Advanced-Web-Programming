a6e9f25e0fb4641368c0a99946c69cf5c054ffca
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-04-04 10:20:09 +0200

    stack

--- Training/JS/stack/app.js
+++ Training/JS/stack/app.js
@@ -4,6 +4,3 @@ push("Rabia");
 push("Sukru");
 
 
-console.log("isEmpty", isEmpty());
-console.log("isFull", isFull());
-console.log("peek", peek());
---
+++ Training/JS/stack/console.js
@@ -0,0 +1,3 @@
+console.log("isEmpty", isEmpty());
+console.log("isFull", isFull());
+console.log("peek", peek());
