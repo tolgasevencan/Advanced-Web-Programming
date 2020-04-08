389105dcd6c8e44a50214d6885e78b3b0b0aacec
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-04-08 20:55:22 +0200

    idk

---
+++ Tolga_Sevencan_AWP_M105_muhasebeci/js/app.js
@@ -0,0 +1,28 @@
+/**
+ * - EXPLANATION -
+ * The user will create a new contact with the form tool on the left of the screen and add it to the table on the right.
+ * When the Save button is pressed, the fields in the form will be reset.
+ * 
+ * There will be a search button on the table on the right.
+ * Search will be made with the name and surname from the search button.
+ */
+
+
+/**
+ * - PROGRAM FLOW -
+ * <INDEX.HTML>
+ * First of all, it is a 'main tag' where the program will be found!
+ * Create two div tag under the main tag! (left and right divs)
+ * The left div will be the registration div.
+ * The right div will be the list div.
+ * Below the right divide are two elements, one for use for search and the other for use for the list.
+ * 
+ * <DOM.JS>
+ * 
+ * <MODEL.JS>
+ * 
+ * <APP.JS>
+ * 
+ * <STYLE.JS>
+ * 
+ */
