78c5c9f46fb6e128303bca2ad0b535fa15102ef0
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-04-09 10:23:03 +0200

    going on

---
+++ Training/cml-dgn-mhsb/app.js
@@ -0,0 +1,31 @@
+class Person {
+    constructor(fName, lName, canton) {
+        this.firstName = fName;
+        this.lastName = lNmae;
+        this.canton = canton;
+    }
+}
+
+class Manager {
+    constructor() {
+        this.submitButton = document.querySelector('input[type = "button"]');
+        this.personForm = document.querySelector('form[name="person-form:]');
+        this.setListeners();
+        this.people = [];
+    }
+    setListeners() {
+        this.submitButton.addEventListener('click', this.onSubmit.bind(this));
+    }
+    onSubmit() {
+        const person = {};
+        Array.form(this.personForm.elements).map(item => {
+            if (item.name){
+                person[item.name] = item.value;
+        )}
+                this.people.push[item.name] = item.value;
+        };
+        console.dir(this.formData);
+    }
+}
+
+new Manager();
