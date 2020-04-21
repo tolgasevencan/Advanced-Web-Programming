20b89ff5b0b4c3f25949d951dff4a5238ebecdca
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-04-21 11:56:58 +0200

    a few new functions have been written

--- Tolga_Sevencan_AWP_M103_manav_osman_II/js/app.js
+++ Tolga_Sevencan_AWP_M103_manav_osman_II/js/app.js
@@ -5,17 +5,25 @@
  * The prices should be written beside the products on the left.
  * On the right side, there will be products purchased.
  * On the right side, the quantities should be written next to the products.
+ * When the product is clicked on the left side showcase, it will be moved to the right side showcase.
  * Total basket amount (total price) should be written under the right section.
  */
 
 /**
  * PROGRAM FLOW
- * - index.html -
- * Create a main tag with the program!
- * Create two divs to be left showcase container and right showcase container!
- * Create a table with products and prices in the window on the left!
- * Create a table with products, quantities, price and total in the window on the rigt!.
- * 
- * - functions.js
- * 
+ * / index.html \
+ * + Create a main tag with the program!
+ * + Create two divs to be left showcase container and right showcase container!
+ * + Create a table with products and prices in the window on the left!
+ * + Create a table with products, quantities, price and total in the window on the rigt!.
+ * =======
+ * / functions.js \
+ * - Write a function that adds products and product prices to the left side showcase.
+ * - Write a function that carries products from the left side showcase to right side showcase.
+ * - Write a function that adds the same products in quantity!
+ * - Write a function that adds up prices!
+ * =======
+ * / app.js \
+ * - Create a products object!
+ * - Create an empty cart object!
  */
--- Tolga_Sevencan_AWP_M103_manav_osman_II/js/functions.js
+++ Tolga_Sevencan_AWP_M103_manav_osman_II/js/functions.js
@@ -1,1 +1,12 @@
-//
+// Function that adds products
+
+function addProducts(pArray, id) {
+    let products = document.querySelector(id);
+    products.innerHTML = "";
+    pArray.forEach(item => {
+        let li = document.createElement("li");
+        let liText = document.createTextNode(item);
+        li.appendChild(liText);
+        products.appendChild(li);
+    });
+}
