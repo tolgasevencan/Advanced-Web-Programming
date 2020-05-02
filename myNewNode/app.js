e76cc1f63257639295646866102a975807d97e22
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-05-02 14:00:34 +0200

    nodejs working

--- myNewNode/app.js
+++ myNewNode/app.js
@@ -1,3 +1,30 @@
+/*import {
+    createServer
+} from 'http';
+import {
+    calculator
+} from "./calculator"
+
+const hostname = '127.0.0.1';
+const port = process.argv[3];
+
+const server = createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(process.argv[2] + calculator(3, 4));
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+*/
+
+const http = require('http');
+const {
+    mul
+} = require('./calculator');
+
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -6,7 +33,7 @@ const port = process.argv[3];
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end(process.argv[2]);
+    res.end(process.argv[2] + " " + mul(2, 3));
 });
 
 server.listen(port, hostname, () => {
---
+++ myNewNode/calculator.js
@@ -0,0 +1,4 @@
+module.exports.mul = function multiply(a, b) {
+    return a * b;
+}
+
