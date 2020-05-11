a9f584bd51028282ab2d25eea94f51112767defd
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-05-11 18:26:04 +0200

    creating some files for classes

--- Tolga_Sevencan_AWP_M105_katil_ismet/app.js
+++ Tolga_Sevencan_AWP_M105_katil_ismet/app.js
@@ -2,9 +2,15 @@
  * USER INTERACTION
  * We are asked to have a customer list that they want someone to be killed, when logged in to the application.
  * We are asked to view the orders (people to be killed) of a customer, when we click on any customer.
- * We are asked to see the order's address, when clicking on the orders.
+ * We are asked to see the order's address, when clicking on the order.
+ * We are asked to put a checkbox in order to mark the orders fullfilled in the order list. The fulfilled order should be shown in the list in a faint manner.
  */
 
 /**
  * TECHNICAL DETAILS
+ * - We have three customers. Each of these customers must be a class. (Model Directory)
+ * - We have to create main controller (AppViewController). (View Directory)
+ * - We have to create customer list with a class. (View Directory)
+ * - We have to create an app.js to run the application.
+ * 
  */
