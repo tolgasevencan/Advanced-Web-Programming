aca93dae8e9b3798bdb7eb12dbaf5255b9b12027
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-05-11 08:48:24 +0200

    continue user interaction

---
+++ Tolga_Sevencan_AWP_M105_katil_ismet/app.js
@@ -0,0 +1,10 @@
+/**
+ * USER INTERACTION
+ * We are asked to have a customer list that they want someone to be killed, when logged in to the application.
+ * We are asked to view the orders (people to be killed) of a customer, when we click on any customer.
+ * We are asked to see the order's address, when clicking on the orders.
+ */
+
+/**
+ * TECHNICAL DETAILS
+ */
