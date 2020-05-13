544f1784747b6b03bb57b76da164f07c8783617f
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-05-13 17:45:03 +0200

    protractor test finished

--- Tolga_Sevencan_AWP_M109_protractor_browser_methods/spec.js
+++ Tolga_Sevencan_AWP_M109_protractor_browser_methods/spec.js
@@ -1,22 +1,42 @@
-/*
 describe('Compare websites by resolution', function () {
+
+
     it('i dk yet', function () {
+        browser.waitForAngularEnabled(false);
         browser.get('https://www.pluralsight.com/');
-        browser.sleep(3000);
+        browser.getTitle().then(function (title) {
+            console.log(title);
+        });
+        browser.sleep(7000);
     });
-});
-*/
-describe('Compare websites by resolution', function () {
+
     it('i dk yet', function () {
+        browser.waitForAngularEnabled(false);
         browser.get('https://www.udemy.com/');
+        browser.manage().window().setSize(1920, 1080);
+        browser.getTitle().then(function (title) {
+            console.log(title);
+        });
         browser.sleep(7000);
-        browser.executeScript('window.moveTo(300, 400);' +
-            'window.resizeTo(screen.width, screen.height);');
-        /*
+    });
+
+    it('i dk yet', function () {
+        browser.waitForAngularEnabled(false);
         browser.get('https://www.linkedin.com/');
+        browser.manage().window().maximize();
+        browser.getTitle().then(function (title) {
+            console.log(title);
+        });
         browser.sleep(7000);
+    });
+
+    it('i dk yet', function () {
+        browser.waitForAngularEnabled(false);
         browser.get('https://www.coursera.org/');
+        browser.manage().window().setSize(1400, 800);
+        browser.getTitle().then(function (title) {
+            console.log(title);
+        });
         browser.sleep(7000);
-        */
     });
 });
