965478a955337cff36a5ecf74c33acfedc60724d
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-05-13 07:21:22 +0200

    starting to work with protractor

--- Tolga_Sevencan_AWP_M109_protractor_browser_methods/spec.js
+++ Tolga_Sevencan_AWP_M109_protractor_browser_methods/spec.js
@@ -2,20 +2,11 @@ describe('Compare websites', function () {
     it('should add a todo', function () {
         browser.get('https://www.pluralsight.com/');
         browser.sleep(7000);
-    // It's not working. (Code 135)
-
-        /*
-        element(by.model('todoList.todoText')).sendKeys('write first protractor test');
-        element(by.css('[value="add"]')).click();
-
-        var todoList = element.all(by.repeater('todo in todoList.todos'));
-        expect(todoList.count()).toEqual(3);
-        expect(todoList.get(2).getText()).toEqual('write first protractor test');
-
-        // You wrote your first test, cross it off the list
-        todoList.get(2).element(by.css('input')).click();
-        var completedAmount = element.all(by.css('.done-true'));
-        expect(completedAmount.count()).toEqual(2);
-        */
+        browser.get('https://www.udemy.com/');
+        browser.sleep(7000);
+        browser.get('https://www.linkedin.com/');
+        browser.sleep(7000);
+        browser.get('https://www.coursera.org/');
+        browser.sleep(7000);
     });
 });
