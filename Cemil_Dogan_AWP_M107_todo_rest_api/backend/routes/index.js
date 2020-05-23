280ca7cceeeac9d1f7e06e9865cb120b867d83e0
Tolga SEVENCAN <57031726+tolgasevencan@users.noreply.github.com>
2020-05-23 07:10:33 +0200

    node modules installed

--- Cemil_Dogan_AWP_M107_todo_rest_api/backend/routes/index.js
+++ Cemil_Dogan_AWP_M107_todo_rest_api/backend/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    /**
+     * 
+     * ...
+     */
+    res.status(200).send({ title: 'Express' });
 });
 
 module.exports = router;
